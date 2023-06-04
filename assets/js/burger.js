bd85c1df0f18a95d87cf0676e684efb40f8fab4c
Bakerman <danil.metelkin.2003@mail.ru>
2023-06-04 06:06:15 +0600

    fix

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,9 +1,11 @@
 let body = document.querySelector("body");
 let burger = document.querySelector(".header__burger");
 let midPart = document.querySelector(".header__part_mid");
+let leftPart = document.querySelector(".header__part_left");
 
 burger.addEventListener("click", () => {
   body.classList.toggle("scroll_off");
   burger.classList.toggle("active");
   midPart.classList.toggle("active");
+  leftPart.classList.toggle("active");
 });
