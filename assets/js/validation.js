43fd786680f3a0b5705d4bda82ca341ea29d053a
Bakerman <danil.metelkin.2003@mail.ru>
2023-06-05 10:31:25 +0600

    add form for registration, authorization, add validation on client side for this forms, and also

---
+++ assets/js/validation.js
@@ -0,0 +1,34 @@
+let arrInputs = document.querySelectorAll(".registration__input");
+let regBtn = document.querySelector(".registration__button");
+let pass = document.querySelector(".registration__input_password");
+let passRepeat = document.querySelector(".registration__input_password-repeat");
+let counter = 0;
+
+if (counter === 0) {
+  for (i = 0; i < arrInputs.length; i++) {
+    arrInputs[i].value.length < 1 ? counter++ : counter--;
+    console.log(arrInputs[i].value);
+    console.log(typeof arrInputs[i].value);
+    console.log(counter);
+  }
+
+  counter === -arrInputs.length
+    ? (regBtn.disabled = false)
+    : (regBtn.disabled = true);
+  counter = 0;
+}
+
+for (i = 0; i < arrInputs.length; i++) {
+  arrInputs[i].addEventListener("change", () => {
+    counter = 0;
+    for (i = 0; i < arrInputs.length; i++) {
+      arrInputs[i].value.length < 1 ? counter++ : counter--;
+      console.log(arrInputs[i].value);
+      console.log(typeof arrInputs[i].value);
+      console.log(counter);
+    }
+    counter === -arrInputs.length && pass.value === passRepeat.value
+      ? ((regBtn.disabled = false), console.log("just do it"))
+      : ((regBtn.disabled = true), console.log("fuck off bich"));
+  });
+}
